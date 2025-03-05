f1a76bceeda0f06efaf96b75582e0dedf2375e63
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-05 19:36:27 -0300

    Listar top 5

--- src/main/java/br/com/alura/screenmatchweb/controller/SerieControlador.java
+++ src/main/java/br/com/alura/screenmatchweb/controller/SerieControlador.java
@@ -1,32 +1,32 @@
 package br.com.alura.screenmatchweb.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.screenmatchweb.dto.SerieDTO;
-import br.com.alura.screenmatchweb.model.Serie;
-import br.com.alura.screenmatchweb.repository.SerieRepository;
+import br.com.alura.screenmatchweb.service.SerieService;
 
 @RestController
 @RequestMapping("/series")
 public class SerieControlador {
 
-	private SerieRepository repository;
+	private SerieService serieService;
 
-	public SerieControlador(SerieRepository repository) {
-		this.repository = repository;
+	public SerieControlador(SerieService serieService) {
+		this.serieService = serieService;
 	}
 	
 	@GetMapping("/listar")
 	public List<SerieDTO> listarSeries() {
-		return repository.findAll().stream()
-				.map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
-						s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-				.collect(Collectors.toList());
+		return serieService.listarSeries();
+	}
+	
+	@GetMapping("/top5")
+	public List<SerieDTO> listarTop5Series() {
+		return serieService.listarTop5();
 	}
 	
 }
---
+++ src/main/java/br/com/alura/screenmatchweb/service/SerieService.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatchweb.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import br.com.alura.screenmatchweb.dto.SerieDTO;
+import br.com.alura.screenmatchweb.model.Serie;
+import br.com.alura.screenmatchweb.repository.SerieRepository;
+
+@Service
+public class SerieService {
+
+	private SerieRepository serieRepository;
+
+	public SerieService(SerieRepository serieRepository) {
+		this.serieRepository = serieRepository;
+	}
+	
+	public List<SerieDTO> listarSeries() {
+		return converterSerieDTO(serieRepository.findAll());
+	}
+
+	public List<SerieDTO> listarTop5() {
+		return converterSerieDTO(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+	}
+	
+	private List<SerieDTO> converterSerieDTO(List<Serie> series) {
+		return series.stream()
+				.map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+						s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+				.collect(Collectors.toList());
+	}
+}
